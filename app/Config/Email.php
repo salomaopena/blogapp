8cddd6a6d5f8487ac9eda49802a0e92ecf79444d
Salomão Pena <spenna.live@gmail.com>
2026-07-18 11:15:19 -0300

    Aula 3: Shield instaldo com o login e registro funcionando

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'reply@blogapp.test';
+    public string $fromName   = 'BlogApp';
     public string $recipients = '';
 
     /**
